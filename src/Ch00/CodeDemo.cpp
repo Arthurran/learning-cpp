935ec98b1af285a9486f5cae1a15662993d00509
Arthurran <arthurrangelp@gmail.com>
2025-03-18 18:30:27 -0300

    Inicializando o repositorio

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -1,5 +1,5 @@
 // Learning C++ 
-// Exercise 00_03
+// Exercise 00_03 modified
 // Using the exercise files on GitHub, by Eduardo Corpeño 
 
 #include <iostream>
@@ -19,3 +19,6 @@ int main(){
     std::cout << std::endl << std::endl;
     return (0);
 }
+
+
+//Testing
